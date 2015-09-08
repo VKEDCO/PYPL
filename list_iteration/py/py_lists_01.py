8030066613cc3300cbe93e667cdeaa704a1c4e89
vkedco <vladimir.kulyukin@gmail.com>
2015-09-08 17:33:45 -0600

    Create py_lists_01.py

---
+++ list_iteration/py/py_lists_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+lstx = [1, 2, 3]
+print 'lstx[0]', '=', lstx[0]
+print 'lstx[1]', '=', lstx[1]
+print 'lstx[2]', '=', lstx[2]
+
+lsty = [1, 'one', 2, 'two']
+print 'lsty[0]', '=', lsty[0]
+print 'lsty[1]', '=', lsty[1]
+print 'lsty[2]', '=', lsty[2]
+print 'lsty[3]', '=', lsty[3]
