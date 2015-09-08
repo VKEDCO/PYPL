7629a1e24811a99fa1cd6384d009b06c4e257b48
vkedco <vladimir.kulyukin@gmail.com>
2015-09-08 17:36:07 -0600

    Create qw.pl

---
+++ list_iteration/pl/qw.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @lstx = qw/1 one 2 two 3 three/;
+print "$lstx[0] "; print "$lstx[1]\n";
+print "$lstx[2] "; print "$lstx[3]\n";
+print "$lstx[4] "; print "$lstx[5]\n";
+
+my @lstx2 = qw{1 one 2 two 3 three};
+print "$lstx2[0] "; print "$lstx2[1]\n";
+print "$lstx2[2] "; print "$lstx2[3]\n";
+print "$lstx2[4] "; print "$lstx2[5]\n";
