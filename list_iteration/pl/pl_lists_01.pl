bba3a67885bccda5521e8a2b7fc301f25b845aa4
vkedco <vladimir.kulyukin@gmail.com>
2015-09-08 17:34:56 -0600

    Create pl_lists_01.pl

---
+++ list_iteration/pl/pl_lists_01.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @lstx = (1, 2, 3);
+print 'lstx[0]', '=', $lstx[0], "\n";
+print 'lstx[1]', '=', $lstx[1], "\n";
+print 'lstx[2]', '=', $lstx[2], "\n";
+
+my @lsty = (1, 'one', 2, 'two');
+print 'lsty[0]', '=', $lsty[0], "\n";
+print 'lsty[1]', '=', $lsty[1], "\n";
+print 'lsty[2]', '=', $lsty[2], "\n";
+print 'lsty[3]', '=', $lsty[3], "\n";
