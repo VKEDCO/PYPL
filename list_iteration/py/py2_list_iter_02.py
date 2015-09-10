a706ea037b9627458822f6acafda14172c0c5d05
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 18:30:56 -0600

    Create py2_list_iter_02.py

---
+++ list_iteration/py/py2_list_iter_02.py
@@ -0,0 +1,17 @@
+## list iteration methods in Py
+## author: Vladimir Kulyukin
+
+lst = [1, 2, 3, 4]
+
+## use lazy range from 0 upto the length of lst - 1.
+## index into a specific spot in lst.
+for i in xrange(0, len(lst)):
+    print lst[i]
+print 'i=', i, "\n"  # i is bound to its last value in for-loop
+
+## the above for-loop and print produce the following output:
+## 1
+## 2
+## 3
+## 4
+## i= 3
