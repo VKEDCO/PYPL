6047128ad1c09ccb9b4e80c8657792faf9db180c
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 18:42:51 -0600

    Create pl_list_iter_01.pl

---
+++ list_iteration/pl/pl_list_iter_01.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+## If you are on linux, do "which perl" to ensure
+## that the perl interpreter is installed in /usr/bin/perl.
+## Change the shabang statement above if necessary.
+##
+## author: Vladimir Kulyukin
+
+use strict;
+use warnings;
+
+my @lst = (1, 2, 3, 4);
+
+## Method 1
+print "Method 01\n";
+for(my $i = 0; $i <= $#lst; $i++) {
+  print "\$lst[$i] = $lst[$i]\n";
+}
+print "\n";
+
+## the output of the above code segment:
+## Method 01
+## $lst[0] = 1
+## $lst[1] = 2
+## $lst[2] = 3
+## $lst[3] = 4
