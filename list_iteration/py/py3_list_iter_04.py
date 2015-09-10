71d320535f4fc1019990047e03b92aa1f5fbbcd8
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 18:38:36 -0600

    Create py3_list_iter_04.py

---
+++ list_iteration/py/py3_list_iter_04.py
@@ -0,0 +1,17 @@
+## list iteration methods in Py
+## author: Vladimir Kulyukin
+
+lst = [1, 2, 3, 4]
+
+## destructively modifying lst by setting them to
+## their square roots.
+for i in range(0, len(lst)):
+    lst[i] **= .5
+
+for x in lst: print(x)
+
+## output of the last for-loop is
+## 1.0
+## 1.41421356237
+## 1.73205080757
+## 2.0
