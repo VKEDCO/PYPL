86511c561b7acc26c0720ae1ac8c794a795d77fe
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 18:31:25 -0600

    Create py2_list_iter_03.py

---
+++ list_iteration/py/py2_list_iter_03.py
@@ -0,0 +1,10 @@
+## list iteration methods in Py
+## author: Vladimir Kulyukin
+
+lst = [1, 2, 3, 4]
+
+## non-lazy version of the index range. produces the same
+## output as the for loop with xrange above.
+for i in range(0, len(lst)):
+    print lst[i]
+print 'i=', i, "\n"
