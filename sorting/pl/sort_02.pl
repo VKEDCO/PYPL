617111a5596c23c5b47b0efc928472b79f7db57d
vkedco <vladimir.kulyukin@gmail.com>
2015-09-22 19:09:16 -0600

    Update sort_02.pl

--- sorting/pl/sort_02.pl
+++ sorting/pl/sort_02.pl
@@ -3,30 +3,40 @@
 use strict;
 use warnings;
 
-## custom sorting in Perl
-## output is
-## 11 1 2 24 3
-## @sorted_strings = 1 11 2 24 3
-## @sorted_numbers = 1 2 3 11 24
-## @sorted_words = a b aa bb aaa bbb
-## 0 0 1 0 1 
-## 4 0 1 
-## 1 2 3 5 
-##
-## bugs to vladimir dot kulyukin at gmail dot com
-
-my @numbers = (11, 1, 2, 24, 3);
-print "@numbers\n";
+################################################
+#
+# illustrates that the custom sorts in PL.
+#
+# author: vladimir kulyukin
+#
+# ... nor shall they enter the Garden, until
+# the camel enters the eye of the needle.
+#################################################
 
 ## explictly comparing numbers as ASCII strings, which is
 ## what the cmp operator does. the scalars $a and $b
 ## are default arguments.
-my @sorted_strings = sort { $a cmp $b } @numbers;
-print "\@sorted_strings = @sorted_strings\n";
+sub sorting_test_4 {
+    print "**** SORTING TEST 4 ****\n\n";
+    my @numbers = (3, 1, 11, 2, 24);
+    print '@numbers before sort:', "\t@numbers\n";
+    my @sorted_numbers = sort { $a cmp $b } @numbers;
+    print '@sorted_numbers:', "\t@sorted_numbers\n";
+    print '@numbers after sort:', "\t@numbers\n";
+    print "\n";
+}
 
-## explict numeric comparison
-my @sorted_numbers = sort { $a <=> $b } @numbers;
-print "\@sorted_numbers = @sorted_numbers\n";
+## explict numeric comparison, i.e., scalars
+## are sorted as numbers.
+sub sorting_test_5 {
+    print "**** SORTING TEST 5 ****\n\n";
+    my @numbers = (11, 1, 2, 24, 3);
+    print '@numbers before sort:', "\t@numbers\n";
+    my @sorted_numbers = sort { $a <=> $b } @numbers;
+    print '@sorted_numbers:', "\t@sorted_numbers\n";
+    print '@numbers before sort:', "\t@numbers\n";
+    print "\n";
+}
 
 ## custom function that compares two scalar strings
 ## by length
@@ -44,10 +54,16 @@ sub str_len_cmp {
   }
 }
 
-my @words = qw(aa bb a aaa b bbb);
-## sort strings by length
-my @sorted_words = sort { str_len_cmp($a, $b) } @words;
-print "\@sorted_words = @sorted_words\n";
+## scalar strings are sorted by length
+sub sorting_test_6 {
+    print "**** SORTING TEST 6 ****\n\n";
+    my @words = qw(aa bb a aaa b bbb);
+    print '@words before sort:', "\t@words\n";
+    ## sort strings by length
+    my @sorted_words = sort { str_len_cmp($a, $b) } @words;
+    print '@sorted_words:     ', "\t@sorted_words\n";
+    print '@words after  sort:', "\t@words\n";
+}
 
 ## function that comparies two lists by the
 ## sum of their elemements.
@@ -66,21 +82,24 @@ sub list_sum_cmp {
   else {
     return 0;
   }
-  
 }
 
-my @sub_list_01 = (1, 2, 3, 5);
-my @sub_list_02 = (4, 0, 1);
-my @sub_list_03 = (0, 0, 1, 0, 1);
-## a list of 3 list references
-my @two_d_list = (\@sub_list_01, \@sub_list_02, \@sub_list_03);
-## sorting list references by the sums of their referent
-## lists.
-my @sorted_2d_list = sort { list_sum_cmp($a, $b) } @two_d_list;
-
-## displaying the result of soring by sum.
-foreach(@sorted_2d_list) {
-  my @sub_list = @{$_};
-  print "$_ " foreach(@sub_list);
-  print "\n";
+sub sorting_test_7 {
+    print "****** SORTING TEST 7 *******\n\n";
+    my @sub_list_01 = (1, 2, 3, 5);
+    my @sub_list_02 = (4, 0, 1);
+    my @sub_list_03 = (0, 0, 1, 0, 1);
+    ## a list of 3 list references
+    my @list_of_lists = (\@sub_list_01, \@sub_list_02, \@sub_list_03);
+    ## sorting list references by the sums of their referent
+    ## lists.
+    my @sorted_list = sort { list_sum_cmp($a, $b) } @list_of_lists;
+    ## displaying the result of soring by sum.
+    foreach(@sorted_list) {
+	my @sub_list = @{$_};
+	print "$_ " foreach(@sub_list);
+	print "\n";
+    }
 }
+
+sorting_test_7;
