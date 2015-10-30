3fd5c445af1c4735c657d134dd35fa19ac8c7a6b
vkedco <vladimir.kulyukin@gmail.com>
2015-10-30 17:42:32 -0600

    Delete gte_stdin_threshold.py

--- file_processing/gte_stdin_threshold.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-## print integers from STDIN that are >= sys.argv[1]
-import sys
-thresh = int(sys.argv[1])
-for n in [int(x) for x in sys.stdin.readlines() if int(x) >= thresh]:
-    print n
