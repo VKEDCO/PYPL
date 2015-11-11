5cf0239807d701b3079f7ac119d10392bace7244
vkedco <vladimir.kulyukin@gmail.com>
2015-11-10 17:15:42 -0700

    Update AddTwoEx.pm

--- packages_modules/pl/MyMathPL/Add/AddTwoEx.pm
+++ packages_modules/pl/MyMathPL/Add/AddTwoEx.pm
@@ -1,12 +1,10 @@
 #!/usr/bin/perl
 
-## bugs to vladimir dot kulyukin at gmail dot com
-
 package MyMathPL::Add::AddTwoEx;
 
 ## this is the root directory where MyMathPL folder
 ## is placed. Change it accordingly.
-use lib "d:/Dropbox/teaching/PythonPerl/s13/lectures/18/";
+use lib 'C:/Users/Vladimir/programming/pl/MyMathPL/';
 
 use strict;
 use warnings;
@@ -29,6 +27,8 @@ sub info {
 }
 
 1;
+
+
 
 
 
