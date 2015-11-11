bcead11cfa09f7e6f003f3dd1b0262448896be74
vkedco <vladimir.kulyukin@gmail.com>
2015-11-10 17:27:46 -0700

    Create mod_mult3.py

---
+++ packages_modules/py/MyMathPy/pack_mult/mod_mult3.py
@@ -0,0 +1,9 @@
+import mod_constants
+
+def mult3(x, y, z): return x * y * z
+
+## function that prints module's info.
+def info():
+    print 'This is mod_mult3 module of ', \
+          mod_constants.app_name, ' application '\
+          ' version ', mod_constants.version
