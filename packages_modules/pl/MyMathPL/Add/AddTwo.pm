5f36966b8f86809c0092b0d098c53c5ce0c92025
vkedco <vladimir.kulyukin@gmail.com>
2015-11-10 17:15:04 -0700

    Update AddTwo.pm

--- packages_modules/pl/MyMathPL/Add/AddTwo.pm
+++ packages_modules/pl/MyMathPL/Add/AddTwo.pm
@@ -4,15 +4,14 @@ package MyMathPL::Add::AddTwo;
 
 ## this is the root directory where MyMathPL folder
 ## is placed. Change it accordingly.
-## bugs to vladimir dot kulyukin at gmail dot com
-use lib "/home/vladimir/Dropbox/teaching/PythonPerl/s13/lectures/18/";
+use lib 'C:/Users/Vladimir/programming/pl/MyMathPL/';
 
 use strict;
 use warnings;
 use MyMathPL::Constants;
 
-sub add2 {
-  return $_[0] + $_[1];
+sub add2 { 
+    return $_[0] + $_[1]; 
 }
 
 sub info {
