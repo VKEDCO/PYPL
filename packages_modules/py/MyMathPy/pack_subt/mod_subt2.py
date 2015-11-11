8abf57957ff182167896ae30df76c358dd519dd5
vkedco <vladimir.kulyukin@gmail.com>
2015-11-10 17:26:10 -0700

    Create mod_subt2.py

---
+++ packages_modules/py/MyMathPy/pack_subt/mod_subt2.py
@@ -0,0 +1,8 @@
+import mod_constants
+
+def subt2(x, y): return x - y
+
+def info():
+    print 'This is mod_subt2 module from of', \
+          mod_constants.app_name, \
+          ' version ', mod_constants.version
