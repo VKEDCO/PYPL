7ee08fb56daa9a49b644791ad4b5a867b3ef15c6
vkedco <vladimir.kulyukin@gmail.com>
2015-11-10 17:10:24 -0700

    Update mod_subt_test.pl

--- packages_modules/pl/MyMathPL/mod_subt_test.pl
+++ packages_modules/pl/MyMathPL/mod_subt_test.pl
@@ -1,38 +1,38 @@
 #!/usr/bin/perl
 
-## bugs to vladimir dot kulyukin at gmail dot com
+######################################
+# @author: vladimir kulyukin
+######################################
 
-use lib "/home/vladimir/Dropbox/teaching/PythonPerl/s13/lectures/18/";
+use lib 'C:/Users/Vladimir/programming/pl/';
 use warnings;
 use strict;
 use MyMathPL::Subt::SubtTwo;
 use MyMathPL::Subt::SubtThree;
 use MyMathPL::Subt::SubtFour;
 
+my $diff1 = 0;
+my $diff2 = 0;
 ############# Testing MyMathPL::Subt::SubtTwo ######################
 MyMathPL::Subt::SubtTwo::info();
-my $subt2_n01 = MyMathPL::Subt::SubtTwo::subt2(12, 10);
-my $subt2_n02 = MyMathPL::Subt::SubtTwo::subt2(15.5, 10.2);
-print $subt2_n01, "\n";
-print $subt2_n02, "\n";
+$diff1 = MyMathPL::Subt::SubtTwo::subt2(12, 10);
+$diff2 = MyMathPL::Subt::SubtTwo::subt2(15.5, 10.2);
+print $diff1, "\n";
+print $diff2, "\n";
 print "\n";
 
 ############# Testing MyMathPL::Subt::SubtThree ######################
 MyMathPL::Subt::SubtThree::info();
-my $subt3_n01 = MyMathPL::Subt::SubtThree::subt3(15, 5, 3);
-my $subt3_n02 = MyMathPL::Subt::SubtThree::subt3(15.5, 5.5, 3.5);
-print $subt3_n01, "\n";
-print $subt3_n02, "\n";
+$diff1 = MyMathPL::Subt::SubtThree::subt3(15, 5, 3);
+$diff2 = MyMathPL::Subt::SubtThree::subt3(15.5, 5.5, 3.5);
+print $diff1, "\n";
+print $diff2, "\n";
 print "\n";
 
 ############# Testing MyMathPL::Subt::SubtFour ######################
 MyMathPL::Subt::SubtFour::info();
-my $subt4_n01 = MyMathPL::Subt::SubtFour::subt4(15, 5, 3, 2);
-my $subt4_n02 = MyMathPL::Subt::SubtFour::subt4(15.5, 5.5, 3.5, 2.5);
-print $subt4_n01, "\n";
-print $subt4_n02, "\n";
+$diff1 = MyMathPL::Subt::SubtFour::subt4(15, 5, 3, 2);
+$diff2 = MyMathPL::Subt::SubtFour::subt4(15.5, 5.5, 3.5, 2.5);
+print $diff1, "\n";
+print $diff2, "\n";
 print "\n";
-
-
-
-
