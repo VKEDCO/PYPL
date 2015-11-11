d92fdc1db1e7c14eb136424852b5ad45b61d0145
vkedco <vladimir.kulyukin@gmail.com>
2015-11-10 17:11:11 -0700

    Update mod_mult_test.pl

--- packages_modules/pl/MyMathPL/mod_mult_test.pl
+++ packages_modules/pl/MyMathPL/mod_mult_test.pl
@@ -1,38 +1,30 @@
 #!/usr/bin/perl
 
-######################################
+#################################
 # @author: vladimir kulyukin
-######################################
+#################################
 
+## change this root accordingly.
 use lib 'C:/Users/Vladimir/programming/pl/';
 use warnings;
 use strict;
-use MyMathPL::Subt::SubtTwo;
-use MyMathPL::Subt::SubtThree;
-use MyMathPL::Subt::SubtFour;
+use MyMathPL::Mult::MultTwo;
+use MyMathPL::Mult::MultFour;
 
-my $diff1 = 0;
-my $diff2 = 0;
-############# Testing MyMathPL::Subt::SubtTwo ######################
-MyMathPL::Subt::SubtTwo::info();
-$diff1 = MyMathPL::Subt::SubtTwo::subt2(12, 10);
-$diff2 = MyMathPL::Subt::SubtTwo::subt2(15.5, 10.2);
-print $diff1, "\n";
-print $diff2, "\n";
+my $prod1 = 0;
+my $prod2 = 0;
+############# Testing MyMathPL::Mult::MultTwo ######################
+MyMathPL::Mult::MultTwo::info();
+$prod1 = MyMathPL::Mult::MultTwo::mult2(10, 12); 
+$prod2 = MyMathPL::Mult::MultTwo::mult2(10.5, 12.5);
+print $prod1, "\n";
+print $prod2, "\n";
 print "\n";
 
-############# Testing MyMathPL::Subt::SubtThree ######################
-MyMathPL::Subt::SubtThree::info();
-$diff1 = MyMathPL::Subt::SubtThree::subt3(15, 5, 3);
-$diff2 = MyMathPL::Subt::SubtThree::subt3(15.5, 5.5, 3.5);
-print $diff1, "\n";
-print $diff2, "\n";
-print "\n";
-
-############# Testing MyMathPL::Subt::SubtFour ######################
-MyMathPL::Subt::SubtFour::info();
-$diff1 = MyMathPL::Subt::SubtFour::subt4(15, 5, 3, 2);
-$diff2 = MyMathPL::Subt::SubtFour::subt4(15.5, 5.5, 3.5, 2.5);
-print $diff1, "\n";
-print $diff2, "\n";
+############# Testing MyMathPL::Mult::MultFour ######################
+MyMathPL::Mult::MultFour::info();
+$prod1 = MyMathPL::Mult::MultFour::mult4(10, 12, 15, 20);
+$prod2 = MyMathPL::Mult::MultFour::mult4(10.5, 12.5, 15.5, 20.5); 
+print $prod1, "\n";
+print $prod2, "\n";
 print "\n";
