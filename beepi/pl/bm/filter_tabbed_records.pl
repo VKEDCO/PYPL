ee61fe6a98c5403749329d528f09e84b70df148a
vkedco <vladimir.kulyukin@gmail.com>
2015-11-27 15:38:33 -0700

    Create filter_tabbed_records.pl

---
+++ beepi/pl/bm/filter_tabbed_records.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+################################################################
+## @author: vladimir kulyukin
+################################################################    
+use strict;
+use warnings;
+
+my $VAL_INDEX   = shift();
+my $LOWER_BOUND = shift();
+my $UPPER_BOUND = shift();
+while ( <> ) {
+    if ( $_ =~ /\S+/ ) {
+	my @splits = split(/\t/, $_);
+	my $val = $splits[$VAL_INDEX];
+	print $_ if ( $val >= $LOWER_BOUND && $val <= $UPPER_BOUND );
+    }
+}
