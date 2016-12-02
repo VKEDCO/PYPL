ad9e0ef4fe94942ad58d094f2928d8a61cb22e4d
vkedco <vladimir.kulyukin@gmail.com>
2016-12-02 11:08:56 -0700

    Create fnfilter.py

---
+++ mini_search_engine/fnfilter.py
@@ -0,0 +1,26 @@
+ Mini Search Engine: fnfilter.TextFileFilter
+### Module: fnfilter.py ###                                             
+##  Description: TextFileFilter class
+##  of the Mini Search Engine project
+##            
+##  Comments, bugs, and errors to
+##  vladimir dot kulyukin at gmail dot com
+##
+##  Python version: Python 2.7.1
+##
+##  Make sure indentation is correct
+##  when cutting and pasting into
+##  a .py file.                             
+#########################################
+
+import os
+
+class FileFilter:
+    def isValid(self, filename):
+        return True
+
+class TextFileFilter(FileFilter):
+    def isValid(self, filename):
+        return (os.path.isfile(filename) and\
+               (filename.endswith('.txt') or\
+                filename.endswith('.TXT')))
