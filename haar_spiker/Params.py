f409959e7900886a9edfb0aab3266030e6f944a8
vkedco <vladimir.kulyukin@gmail.com>
2017-02-14 10:32:37 -0700

    Create Params.py

---
+++ haar_spiker/Params.py
@@ -0,0 +1,51 @@
+#########################
+# module: Params.py
+# author: Vikas Reddy
+#########################
+
+class Params:
+    def __init__(self):
+        pass
+
+    '''
+    # 12oct2016
+    ROI_START_ROW = 184
+    ROI_END_ROW = 240
+    ROI_START_COL = 84
+    ROI_END_COL = 295
+    '''
+
+
+    # 8nov2016
+    '''
+    ROI_START_ROW = 213
+    ROI_END_ROW = 240
+    ROI_START_COL = 129
+    ROI_END_COL = 215
+    '''
+
+    # 12nov2016
+    '''
+    ROI_START_ROW = 183
+    ROI_END_ROW = 240
+    ROI_START_COL = 40
+    ROI_END_COL = 260
+    '''
+
+    # 6jan2017
+    '''
+    ROI_START_ROW = 55
+    ROI_END_ROW = 133
+    ROI_START_COL = 65
+    ROI_END_COL = 352
+    '''
+
+    # 7jan2017
+
+    ROI_START_ROW = 26
+    ROI_END_ROW = 122
+    ROI_START_COL = 43
+    ROI_END_COL = 338
+
+    ROI_HEIGHT = ROI_END_ROW - ROI_START_ROW
+    ROI_WIDTH = ROI_END_COL - ROI_START_COL
